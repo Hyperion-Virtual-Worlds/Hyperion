d51de47c4f583e3b11c8732d86afd7c5597b891e
EmperorStarfinder <emperor@secondgalaxy.com>
2021-10-11 15:06:04 -0400

    Add the new Hyperion.Common project
    NOTE: This is only the template files for the project.  they will change as the project is fully written.

--- Hyperion/Hyperion.Common/HttpServer/HttpRequestBodyStream.cs
+++ Hyperion/Hyperion.Common/HttpServer/HttpRequestBodyStream.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hyperion.Common
+namespace Hyperion.Common.HttpServer
 {
-    class Program
+    class HttpRequestBodyStream
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
