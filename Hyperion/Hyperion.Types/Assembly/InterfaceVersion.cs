29f2c64bff6480519c133b12cdf701318d270f2d
NoahStarfinder <noah@secondgalaxy.com>
2021-10-13 18:21:37 -0400

    Initial implementation of Hyperion.Types.Assembly
    Signed-off-by: NoahStarfinder <noah@secondgalaxy.com>

--- Hyperion/Hyperion.Types/Assembly/InterfaceVersion.cs
+++ Hyperion/Hyperion.Types/Assembly/InterfaceVersion.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+// Some parsers fail on Version and think it is a type
+#pragma warning disable IDE0003
+
+using System;
 
 namespace Hyperion.Types.Assembly
 {
-    class InterfaceVersion
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
+    public sealed class InterfaceVersionAttribute : Attribute
     {
+        public string Version { get; }
+
+        public InterfaceVersionAttribute(string version)
+        {
+            this.Version = version;
+        }
     }
 }
