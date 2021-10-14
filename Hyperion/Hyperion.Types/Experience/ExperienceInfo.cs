13732b255926fa0809f7c72ad4cde45d92ecea17
NoahStarfinder <noah@secondgalaxy.com>
2021-10-14 12:26:09 -0400

    Initial implementation of Hyperion.Type.Experience
    NOTE: There are likely some items that need to be in here for the metaverse side of things for EEP and VMM.
    Signed-off-by: NoahStarfinder <noah@secondgalaxy.com>

--- Hyperion/Hyperion.Types/Experience/ExperienceInfo.cs
+++ Hyperion/Hyperion.Types/Experience/ExperienceInfo.cs
@@ -1,12 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <license>
+/// Hyperion Virtual Worlds https://hyperionvirtual.com
+/// is distributed under the terms of the
+/// GNU Affero General Public License v3 with
+/// the following clarification and special exception.
+/// 
+/// Linking this library statically or dynamically with other modules is
+/// making a combined work based on this library. Thus, the terms and
+/// conditions of the GNU Affero General Public License cover the whole
+/// combination.
+/// 
+/// As a special exception, the copyright holders of this library give you
+/// permission to link this library with independent modules to produce an
+/// executable, regardless of the license terms of these independent
+/// modules, and to copy and distribute the resulting executable under
+/// terms of your choice, provided that you also meet, for each linked
+/// independent module, the terms and conditions of the license of that
+/// module. An independent module is a module which is not derived from
+/// or based on this library. If you modify this library, you may extend
+/// this exception to your version of the library, but you are not
+/// obligated to do so. If you do not wish to do so, delete this
+/// exception statement from your version.
+/// </license>
+
+using Hyperion.Types.Grid;
+using Hyperion.Types.StructuredData.Llsd;
+using System;
+using System.IO;
 
 namespace Hyperion.Types.Experience
 {
-    class ExperienceInfo
+    [Flags]
+    public enum ExperiencePropertyFlags
     {
+        None = 0,
+        Invalid = 1 << 0,
+        Privileged = 1 << 3,
+        Grid = 1 << 4,
+        Private = 1 << 5,
+        Disabled = 1 << 6,
+        Suspended = 1 << 7
+    }
+
+    public class ExperienceInfo
+    {
+        public UEI ID = UEI.Unknown;
+        public string Description = string.Empty;
+        public ExperiencePropertyFlags Properties;
+        public UGUI Owner = UGUI.Unknown;
+        public UGUI Creator = UGUI.Unknown;
+        public UGI Group = UGI.Unknown;
+        public RegionAccess Maturity;
+        public string Marketplace = string.Empty;
+        public UUID LogoID = UUID.Zero;
+        public string SlUrl = string.Empty;
+
+        public string ExtendedMetadata
+        {
+            get
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    LlsdXml.Serialize(new Map
+                    {
+                        ["marketplace"] = (AString)Marketplace,
+                        ["logo"] = LogoID
+                    }, ms);
+                    return ms.ToArray().FromUTF8Bytes();
+                }
+            }
+            set
+            {
+                using (MemoryStream ms = new MemoryStream(value.ToUTF8Bytes()))
+                {
+                    IValue iv;
+                    Map m = (Map)LlsdXml.Deserialize(ms);
+                    Marketplace = m.TryGetValue("marketplace", out iv) ? iv.ToString() : string.Empty;
+                    m.TryGetValue("logo", out LogoID);
+                }
+            }
+        }
+
+        public ExperienceInfo()
+        {
+        }
+
+        public ExperienceInfo(ExperienceInfo info)
+        {
+            ID = info.ID;
+            Description = info.Description;
+            Properties = info.Properties;
+            Creator = new UGUI(info.Creator);
+            Owner = new UGUI(info.Owner);
+            Group = new UGI(info.Group);
+            Maturity = info.Maturity;
+            Marketplace = info.Marketplace;
+            LogoID = info.LogoID;
+            SlUrl = info.SlUrl;
+        }
     }
 }
