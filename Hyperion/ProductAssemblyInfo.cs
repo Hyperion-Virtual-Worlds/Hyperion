749a8e5e19b10e5dba24b9e97303466f0051d245
BruceStarfinder <bruce@secondgalaxy.com>
2022-01-09 16:32:36 -0500

    Add ProductAssemblyInfo file
    - General Solution Cleanup
    - Add ProductAssemblyInfo file

---
+++ Hyperion/ProductAssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Hyperion Virtual Worlds")]
+[assembly: AssemblyCompany("Virtual World Research Inc.")]
+[assembly: AssemblyCopyright("Affero GPLv3 License+Link Exception")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+
+#if NO_HYPERION_TYPES
+#else
+[assembly: Hyperion.Types.Assembly.InterfaceVersion("0.0.0.0")]
+#endif
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
